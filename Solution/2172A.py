731d5d5aeeaed0c84ce4601db64ba06741403479
Pm Pranta <heyiampm@gmail.com>
2025-12-15 11:08:09 +0600

    Code Solution

---
+++ Solution/2172A.py
@@ -0,0 +1,11 @@
+# ! ASCII Art Contest.(Code forces problem  01)
+g, c, l = map(int, input().split())
+
+if not (80 <= g <= 100 and 80 <= c <= 100 and 80 <= l <= 100):
+          print("check again")
+          
+elif max(g, c, l) - min(g, c, l) >= 10:
+          print("check again")
+          
+else:
+          print("final", g + c + l - max(g, c, l) - min(g, c, l))
