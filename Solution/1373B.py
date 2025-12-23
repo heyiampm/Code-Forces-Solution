569d9623079a11b1b38407493041686e4124412a
Pm Pranta <heyiampm@gmail.com>
2025-12-23 19:59:06 +0600

    Code Solution

--- Solution/1373B.py
+++ Solution/1373B.py
@@ -1,16 +1,16 @@
 #Problem name:("Game")//(Code forces problem 33)
 
-# t_str = input()
-# if t_str:
-#           t = int(t_str)
-#           for _ in range(t):
-#                     s = input()
-#                     count_0 = s.count('0')
-#                     count_1 = s.count('1')
-#                     total_moves = min(count_0, count_1)
-#                     if total_moves % 2 == 1:
-#                               print("DA")
-#                     else:
-#                               print("NET")
+t_str = input()
+if t_str:
+          t = int(t_str)
+          for _ in range(t):
+                    s = input()
+                    count_0 = s.count('0')
+                    count_1 = s.count('1')
+                    total_moves = min(count_0, count_1)
+                    if total_moves % 2 == 1:
+                              print("DA")
+                    else:
+                              print("NET")
 
 
