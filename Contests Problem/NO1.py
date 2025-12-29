f1aff201555b99f3d79a60705d0c6086cffc8f38
Pm Pranta <heyiampm@gmail.com>
2025-12-30 00:14:39 +0600

    Solution

--- Contests Problem/NO1.py
+++ Contests Problem/NO1.py
@@ -3,7 +3,7 @@
 t = int(input())
 for _ in range(t):
     s = input().strip()
-    if s[0] == 'Y' and s[-1] == 'Y':
+    if s [0] == 'Y' and s[-1] == 'Y':
         print("NO")
     else:
         print("YES")
