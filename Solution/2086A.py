7bbd4d251409c4db0f1488660eea21166b0da5f6
Pm Pranta <heyiampm@gmail.com>
2026-01-02 11:28:08 +0600

    Code Solution

--- Solution/2086A.py
+++ Solution/2086A.py
@@ -1,7 +1,7 @@
 #Problem name:("Cloudberry Jam")//(Code forces problem 13)
 
-t = int(input())
-for _ in range(t):
+p = int(input())
+for _ in range(p):
           n = int(input())
           value = 2 * n
           print(value)
