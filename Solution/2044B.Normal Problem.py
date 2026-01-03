69ec0c5db55a955bfb43f121cdfca8f5839eecc7
Pm Pranta <heyiampm@gmail.com>
2026-01-03 19:51:46 +0600

    Normal Problem Solution

---
+++ Solution/2044B.Normal Problem.py
@@ -0,0 +1,10 @@
+#Problem name:("Normal Problem")//(Code forces problem 04)
+
+t = int(input())
+for _ in range(t):
+          a = input().strip()
+          mirror = {'p':'q', 'q':'p', 'w':'w'}
+          b = '' 
+          for ch in reversed(a):
+                    b += mirror[ch]
+                    print( b )
