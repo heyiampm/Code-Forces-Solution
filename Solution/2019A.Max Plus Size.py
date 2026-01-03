5f2d7cb55a36a254fe2cb3ea8b228e154f4e31ec
Pm Pranta <heyiampm@gmail.com>
2026-01-03 19:40:51 +0600

    Max Plus Size Solution

--- Solution/2019A.Max Plus Size.py
+++ Solution/2019A.Max Plus Size.py
@@ -3,6 +3,6 @@
 for _ in range(int(input())):
           n = int(input())
           a = list(map(int, input().split()))
-          odd_max = max(a[::2]) if n >= 1 else 0
+          odd_max = max(a[::2]) if n >= 1  else 0
           even_max = max(a[1::2]) if n > 1 else 0
           print(max(odd_max + (n+1)//2, even_max + n//2))
