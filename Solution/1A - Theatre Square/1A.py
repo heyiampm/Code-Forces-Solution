a86c2c9eb2a61231a480c8077bc5414b59b336d4
Pm Pranta <heyiampm@gmail.com>
2026-01-05 23:46:09 +0600

    1A - Theatre Square Solution

---
+++ Solution/1A - Theatre Square/1A.py
@@ -0,0 +1,7 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1/A
+
+n, m, a = map(int, input().split())
+tiles_n = (n + a - 1) // a
+tiles_m = (m + a - 1) // a
+
+print(tiles_n * tiles_m)
