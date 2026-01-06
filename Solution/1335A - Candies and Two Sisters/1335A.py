2acc9006bc38d3497b19065e7a8e715a154e9a8b
Pm Pranta <heyiampm@gmail.com>
2026-01-06 10:29:28 +0600

    1335A - Candies and Two Sisters Solution

---
+++ Solution/1335A - Candies and Two Sisters/1335A.py
@@ -0,0 +1,14 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1335/A
+
+no = int(input())
+for i in range(0, no):
+          a = int(input())
+          if a >= 0 and a <= 2:
+                    print(0)
+          else:
+                    if a % 2 == 0:
+                              t = (a/2)-1
+                              print(int(t))
+                    else:
+                              t =int (a/2)
+                              print(t)
