a1970cae02bbbafc812c25fde7999ac1b4a69531
Pm Pranta <heyiampm@gmail.com>
2026-01-07 00:06:50 +0600

    2044A - Easy Problem Solution

---
+++ Solution/2044A - Easy Problem/2044A.py
@@ -0,0 +1,8 @@
+# Problem can be found at https://codeforces.com/problemset/problem/2044/A
+
+t = int(input())
+for _ in range(t):
+          n= int(input())
+          a = 1 <= t <= 99
+          b = 2 <= n <= 100
+          print((n - b))
