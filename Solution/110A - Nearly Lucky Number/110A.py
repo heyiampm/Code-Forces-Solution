92e2171395cf8e07c2538a8b0df6068b17fddb91
Pm Pranta <heyiampm@gmail.com>
2026-01-07 00:25:18 +0600

    110A - Nearly Lucky Number Solution

---
+++ Solution/110A - Nearly Lucky Number/110A.py
@@ -0,0 +1,11 @@
+# Problem can be found at https://codeforces.com/problemset/problem/110/A
+
+n = input()
+count = 0
+for digit in n:
+    if digit == '4' or digit == '7':
+        count += 1
+        if count == 4 or count == 7:
+            print("YES")
+        else:
+            print("NO")
