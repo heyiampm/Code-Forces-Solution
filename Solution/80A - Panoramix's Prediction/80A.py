294b9f21a48c37ccc7fa281f619f14318fb7db6e
Pm Pranta <heyiampm@gmail.com>
2026-01-06 09:30:07 +0600

    80A - Panoramix's Prediction Solution

---
+++ Solution/80A - Panoramix's Prediction/80A.py
@@ -0,0 +1,5 @@
+# Problem can be found at https://codeforces.com/problemset/problem/80/A
+
+p = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+n, m = map(int, input().split())
+print("YES" if p[p.index(n) + 1] == m else "NO")
