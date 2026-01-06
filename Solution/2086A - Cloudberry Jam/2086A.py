aec1d1f97312e0d3dea16eaabe232f5b073cd1c3
Pm Pranta <heyiampm@gmail.com>
2026-01-07 00:08:45 +0600

    2086A - Cloudberry Jam Solution

---
+++ Solution/2086A - Cloudberry Jam/2086A.py
@@ -0,0 +1,7 @@
+# Problem can be found at https://codeforces.com/problemset/problem/2086/A
+
+p = int(input())
+for _ in range(p):
+          n = int(input())
+          value = 2 * n
+          print(value)
