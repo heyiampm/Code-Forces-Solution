fdbdad84ccf17d37132a90e759afa30924e8a1cd
Pm Pranta <heyiampm@gmail.com>
2026-01-07 00:24:37 +0600

    96A - Football Solution

---
+++ Solution/96A - Football/96A.py
@@ -0,0 +1,8 @@
+# Problem can be found at https://codeforces.com/problemset/problem/96/A
+
+s = input().strip()
+
+if '0000000' in s or '1111111' in s:
+          print("YES")
+else:
+          print("NO")
