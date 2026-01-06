9ffd7ce4083ee2585d66946ae459c17ddae3b0f2
Pm Pranta <heyiampm@gmail.com>
2026-01-06 10:35:32 +0600

    2019A - Max Plus Size Solution

---
+++ Solution/2019A - Max Plus Size/2019A.py
@@ -0,0 +1,8 @@
+# Problem can be found at https://codeforces.com/problemset/problem/2019/A
+
+for _ in range(int(input())):
+          n = int(input())
+          a = list(map(int, input().split()))
+          odd_max = max(a[::2]) if n >= 1  else 0
+          even_max = max(a[1::2]) if n > 1 else 0
+          print(max(odd_max + (n+1)//2, even_max + n//2))
