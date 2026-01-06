f689836a09ef9d142536201aff96357baaab315d
Pm Pranta <heyiampm@gmail.com>
2026-01-06 10:20:31 +0600

    705A - Hulk

---
+++ Solution/705A - Hulk/705A.py
@@ -0,0 +1,14 @@
+# Problem can be found at https://codeforces.com/problemset/problem/705/A
+
+def hulk():
+          n = int(input())
+          ans = []
+          for i in range(1, n+1):
+                    if i % 2==1:
+                              ans.append("I hate")
+                    else:
+                              ans.append("I love")
+          final = " that ".join(ans)
+          final += " it"
+          print(final)
+hulk()
