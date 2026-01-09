283d79eb3609d8420b487b2f26bc2d1072fb8e2c
Pm Pranta <heyiampm@gmail.com>
2026-01-09 11:02:35 +0600

    832A - Sasha and Sticks Solution

---
+++ Solution/832A - Sasha and Sticks/832A.py
@@ -0,0 +1,8 @@
+# Problem can be found at https://codeforces.com/problemset/problem/832/A
+import math
+n, k = map(int, input().split())
+
+if (n // k) & 1 == 1:
+          print("YES")
+else:
+          print("NO")
