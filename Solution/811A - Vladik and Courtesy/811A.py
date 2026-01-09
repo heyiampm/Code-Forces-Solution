12c049db0cf5d05adc22615a8c6d0ae587c26b7d
Pm Pranta <heyiampm@gmail.com>
2026-01-09 11:01:36 +0600

    811A - Vladik and Courtesy Solution

---
+++ Solution/811A - Vladik and Courtesy/811A.py
@@ -0,0 +1,17 @@
+# Problem can be found at https://codeforces.com/problemset/problem/811/A
+
+a, b = map(int, input().split())
+
+for i in range(1, int(1e9 + 1)):
+    if i % 2 == 0:
+        if i > b:
+            print("Valera")
+            quit()
+        else:
+            b -= i
+    else:
+        if i > a:
+            print("Vladik")
+            quit()
+        else:
+            a -= i
