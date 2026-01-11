e1a9c1b1d041435bb4e349489abf4a96ad0d1957
Pm Pranta <heyiampm@gmail.com>
2026-01-11 19:27:55 +0600

    1926A - Vlad and the Best of Five Solution

---
+++ Solution/1926A - Vlad and the Best of Five/1926A.py
@@ -0,0 +1,11 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1926/A
+
+t = int(input())
+for _ in range(t):
+          s = input()
+          a = s.count('A')
+          b = s.count('B')
+          if a > b:
+                    print('A')
+          else:
+                    print('B')
