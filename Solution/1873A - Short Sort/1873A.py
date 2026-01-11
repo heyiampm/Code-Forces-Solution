ec762e06eb3d940a530ebdf52aac18751255f63c
Pm Pranta <heyiampm@gmail.com>
2026-01-11 19:25:15 +0600

    1873A - Short Sort Solution

---
+++ Solution/1873A - Short Sort/1873A.py
@@ -0,0 +1,9 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1873/A
+
+t = int(input())
+for _ in range(t):
+          s = input().strip()
+          if s =="bca" or s == "cab":
+                    print("NO")
+          else:
+                    print("YES")
