fca2510e96eac8c220f7b6ddde6c6ccf436afaf0
Pm Pranta <heyiampm@gmail.com>
2026-01-11 19:33:20 +0600

    1971A - My First Sorting Problem Solution

---
+++ Solution/1971A - My First Sorting Problem/1971A.py
@@ -0,0 +1,6 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1971/A
+
+t = int(input())
+for _ in range(t):
+          x,y = (map(int, input().split()))
+          print(min(x,y),max(x,y))
