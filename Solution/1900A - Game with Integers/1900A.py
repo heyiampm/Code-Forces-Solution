0bd8bf182202e9b3713ca5ae33e5697c03960324
Pm Pranta <heyiampm@gmail.com>
2026-01-11 19:26:03 +0600

    1900A - Game with Integers Solution

---
+++ Solution/1900A - Game with Integers/1900A.py
@@ -0,0 +1,10 @@
+# Problem can be found at https://codeforces.com/problemset/problem/1900/A
+
+t = int(input())
+for _ in range(t):
+          n = int(input())
+          
+          if n % 3 == 0:
+                    print("Second")
+          else:
+                    print("First")
