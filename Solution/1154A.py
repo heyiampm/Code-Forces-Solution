a82a75ce14a688c18f59e706d74e99cb317ac603
Pm Pranta <heyiampm@gmail.com>
2026-01-13 08:46:28 +0600

    Delete Repo file

--- Solution/1154A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Restoring Three Numbers")//(Code forces problem 48)
-
-nums = list(map(int, input().split()))
-nums.sort()
-p, q, r, S = nums
-a = S - r
-b = S - q
-c = S - p
-print(a, b, c)
