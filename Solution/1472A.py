4ad2516dc7e1a85f8bbd75c23c6ec3624639890c
Pm Pranta <heyiampm@gmail.com>
2026-01-13 08:51:15 +0600

    Remove

--- Solution/1472A.py
+++
@@ -1,8 +0,0 @@
-#Problem name:("Cards for Friends")//(Code forces problem 47)
-
-for _ in range(int(input())):
-    w,h,n=map(int,input().split())
-    c=1
-    while w%2==0: w//=2; c*=2
-    while h%2==0: h//=2; c*=2
-    print("YES" if c>=n else "NO")
