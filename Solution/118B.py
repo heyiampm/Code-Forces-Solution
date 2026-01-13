b722f138f1a47fea49110aae7359a4f68cbda2be
Pm Pranta <heyiampm@gmail.com>
2026-01-13 08:47:48 +0600

    118B.py Delete

--- Solution/118B.py
+++
@@ -1,5 +0,0 @@
-#Problem name:("Present from Lena")//(Code forces problem 37)
-
-n = int(input())
-for i in list(range(n + 1)) + list(range(n - 1, -1, -1)):
-    print("  " * (n - i) + " ".join(map(str, list(range(i + 1)) + list(range(i - 1, -1, -1)))))
