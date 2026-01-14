ccdd1dee976eab331d6b99ad64cd5314d086a971
Pm Pranta <heyiampm@gmail.com>
2026-01-14 22:46:20 +0600

    Delete

--- Solution/1999A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("A+B Again?")//(Code forces problem 07)
-
-t = int(input())
-for i in range(t):
-          n = int(input())
-          first  = n // 10
-          sec  = n % 10
-          sum_digit = first + sec
-          print(sum_digit)
