3a2e09115fc65a537d49167c8dfaf89638a6c20f
Pm Pranta <heyiampm@gmail.com>
2026-01-14 22:50:48 +0600

    1971A.remove

--- Solution/1971A.py
+++
@@ -1,6 +0,0 @@
-#Problem name:("My First Sorting Problem")//(Code forces problem 09)
-
-t = int(input())
-for _ in range(t):
-          x,y = (map(int, input().split()))
-          print(min(x,y),max(x,y))
