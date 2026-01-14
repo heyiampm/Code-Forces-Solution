d4e01dc6fd33e60124ebfd42a2dd582e1672dd4f
Pm Pranta <heyiampm@gmail.com>
2026-01-14 22:48:04 +0600

    Code delete

--- Solution/1977A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Little Nikita")//(Code forces problem 35)
-
-t = int(input())
-for _ in range(t):
-    n, m = map(int, input().split())
-    if n >= m and (n - m) % 2 == 0:
-        print("Yes")
-    else:
-        print("No")
