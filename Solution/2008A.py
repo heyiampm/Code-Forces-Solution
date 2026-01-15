6f29433b25dee709e7986198fb3a38a62267fe83
Pm Pranta <heyiampm@gmail.com>
2026-01-15 09:24:54 +0600

    Code remove

--- Solution/2008A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Sak_ur_ako's Exam")//(Code forces problem 18)
-
-t = int(input())
-for _ in range(t):
-          a, b = map(int, input().split())
-          if (a + 2*b) % 2 == 0 and (b % 2 == 0 or a >= 2):
-                    print("YES")
-          else:
-                    print("NO")
