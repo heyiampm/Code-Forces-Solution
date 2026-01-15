33aa5c5714ed76597b0b23a6dd6a2c80315ec909
Pm Pranta <heyiampm@gmail.com>
2026-01-15 09:15:55 +0600

    1703A.py delete

--- Solution/1703A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("YES or YES?")//(Code forces problem 27)
-
-t = int(input())
-for _ in range(t):
-          s = input().strip()
-          if s.upper()=="YES":
-                    print("YES")
-          else:
-                    print("NO")
