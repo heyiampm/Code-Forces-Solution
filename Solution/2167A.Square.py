603dd877440e4ea97ea366ee5eec810fc4a0813f
Pm Pranta <heyiampm@gmail.com>
2026-01-16 19:49:15 +0600

    Code delete

--- Solution/2167A.Square.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Square")//(Code forces problem 03)
-
-t = int(input())
-for _ in range(t):
-          sticks = list(map(int, input().split()))
-          if sticks.count(sticks[0]) == 4:
-                    print("Yes")
-          else:
-                    print("NO")
