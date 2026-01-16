73b9d6570fefd4e6a2a42ede6976ff3d6967b095
Pm Pranta <heyiampm@gmail.com>
2026-01-16 19:49:50 +0600

    delete

--- Solution/469A.py
+++
@@ -1,8 +0,0 @@
-#Problem name:("I Wanna Be the Guy")//(Code forces problem 39)
-
-n = int(input())
-levels = set()
-for _ in range(2):
-    a = list(map(int, input().split()))
-    levels |= set(a[1:])
-print("I become the guy." if len(levels) == n else "Oh, my keyboard!")
