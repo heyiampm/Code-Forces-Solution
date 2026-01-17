463b8a032e16ff3ffc3a336cb7eecaed6722c199
Pm Pranta <heyiampm@gmail.com>
2026-01-17 22:20:15 +0600

    217A.py delete

--- Solution/217A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Beautiful Year")//(Code forces problem 24)
-
-year = int(input())
-while True:
-          year += 1
-          s = str(year)
-          if len(set(s)) == len(s):
-                    print(year)
-                    break
