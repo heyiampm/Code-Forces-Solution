cd00c93127c3973ef242a9c3b7abc53d788ca869
Pm Pranta <heyiampm@gmail.com>
2026-01-18 19:45:38 +0600

    Code delete

--- Solution/520A.py
+++
@@ -1,9 +0,0 @@
-#Problem name:("Pangram")//(Code forces problem 43)
-
-t = int(input())
-s = input().lower()
-letter = set(s)
-if len(letter) == 26:
-                    print("Yes")
-else:
-                    print("No")
