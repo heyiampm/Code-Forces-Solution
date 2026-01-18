5f5c8967ba9e0bde51e856b688d22bd785bd50b8
Pm Pranta <heyiampm@gmail.com>
2026-01-18 21:30:01 +0600

    157B - Trace Solution

---
+++ Solution/157B - Trace/157B.py
@@ -0,0 +1,11 @@
+import math
+
+n = int(input())
+l_s = list(map(int, input().split()))
+l_s.sort(reverse = True)
+
+t = 0
+for index in range(n):
+    t += (-1 if index % 2 == 1 else 1) * l_s[index] ** 2
+
+print(t * math.pi)
