a51edb65e757472897d9fce954036932ea7ae8a9
Pm Pranta <heyiampm@gmail.com>
2026-01-31 23:57:27 +0600

    Code

--- Solution/4A - Watermelon/4A.py
+++ Solution/4A - Watermelon/4A.py
@@ -1,4 +1,4 @@
-# Problem can be found at https://codeforces.com/problemset/problem/4/A
+# Problem can be found at https://codeforces.com/problemset/problem/4/A 
 
 w = int (input())
 if w > 2 and w % 2 == 0:
---
+++ Unpush/un Solve/126B - Passeord/126B.py
@@ -0,0 +1,7 @@
+# https://codeforces.com/problemset/problem/126/B
+
+s = input().strip()
+if s == 'fixprefixsuffix':
+          print("fix")
+else:
+          print("Just a legend")
