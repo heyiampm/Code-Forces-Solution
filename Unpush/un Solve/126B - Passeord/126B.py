e154bbd71255580ebd049660bbbb6946c4f3a882
Pm Pranta <heyiampm@gmail.com>
2026-02-02 23:44:38 +0600

    COde remove

--- Unpush/un Solve/126B - Passeord/126B.py
+++
@@ -1,7 +0,0 @@
-# https://codeforces.com/problemset/problem/126/B
-
-s = input().strip()
-if s == 'fixprefixsuffix':
-          print("fix")
-else:
-          print("Just a legend")
--- Unpush/un Solve/126B - Passeord/1703A - YES or YES/1703A.py
+++
@@ -1,9 +0,0 @@
-# Problem can be found at https://codeforces.com/problemset/problem/1703/A
-
-t = int(input())
-for _ in range(t):
-          s = input().strip()
-          if s.upper()=="YES":
-                    print("YES")
-          else:
-                    print("NO")
