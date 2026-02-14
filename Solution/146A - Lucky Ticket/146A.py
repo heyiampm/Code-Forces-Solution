060179ac1cf4e6bd3b1e85a2a5f6e0e9833d56c9
Pm Pranta <heyiampm@gmail.com>
2026-02-14 23:31:11 +0600

    146A - Lucky Ticket Solution

---
+++ Solution/146A - Lucky Ticket/146A.py
@@ -0,0 +1,11 @@
+
+n = int(input())
+s = input()
+
+if len(s.replace("4", "").replace("7", "")) != 0:
+    print("NO")
+    quit()
+if sum(int(x) for x in s[:len(s) // 2]) != sum(int(x) for x in s[len(s) // 2:]):
+    print("NO")
+    quit()
+print("YES")
