53af1c9eb1ff077ee8e51986e4383fee3bd4dc64
Pm Pranta <heyiampm@gmail.com>
2026-06-16 09:44:29 +0600

    adding solution 16/6/2026

---
+++ Solution/501A - Contest/Problem.py
@@ -0,0 +1,11 @@
+
+a, b, c, d = map(int, input().split())
+p_m = max(3 / 10 * a, a - (a / 250) * c)
+p_v = max(3 / 10 * b, b - (b / 250) * d)
+
+if p_m > p_v:
+    print("Misha")
+elif p_m < p_v:
+    print("Vasya")
+else:
+    print("Tie")
