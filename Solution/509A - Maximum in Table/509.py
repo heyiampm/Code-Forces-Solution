27f1718cb4eb870fa87dce6872e70ad7f3112fe8
Pm Pranta <heyiampm@gmail.com>
2026-06-16 09:45:35 +0600

    adding solution 16/6/2026

---
+++ Solution/509A - Maximum in Table/509.py
@@ -0,0 +1,8 @@
+s = int(input())
+table = [[1 for _ in range(s)] for _ in range(s)]
+
+for r in range(1, s):
+    for c in range(1, s):
+        table[r][c] = table[r - 1][c] + table[r][c - 1]
+        
+print(table[s - 1][s - 1])
