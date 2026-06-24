39e4281e32d5948ed5871e09c783c93e064f6e1f
Pm Pranta <heyiampm@gmail.com>
2026-06-24 20:44:44 +0600

    Solution\158C - Cd and pwd commands

---
+++ Solution/158C - Cd and pwd commands/158C.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+l_d = list()
+x = 0
+
+for _ in range(n):
+    l_c = list(input().split())
+    
+    if l_c[0] == "pwd":
+        print("/" + "".join(l_d[:x]))
+    else:
+        for c in l_c[1].split("/"):
+            if c == "..":
+                x -= 1
+            elif len(c) != 0:
+                if len(l_d) > x:
+                    l_d[x] = c + "/"
+                else:
+                    l_d.append(c + "/")
+                x += 1
+            else:
+                x = 0
